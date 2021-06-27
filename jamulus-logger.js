fd50c3947607dca6b4fc30237033159b3faff0d9
Colin <cdmahoney@gmail.com>
2021-06-27 18:10:54 +0200

    Improved logging format. Ping servers

--- jamulus-logger.js
+++ jamulus-logger.js
@@ -1,12 +1,42 @@
-var config = require("config");
-var luxon = require("luxon");
-var mqtt = require("mqtt");
+const config = require("config");
+const luxon = require("luxon");
+const mqtt = require("mqtt");
 
-var client  = mqtt.connect(`${config.mqttBroker.host}:${config.mqttBroker.port}`);
+const internals = (function()
+{
+    const getContext = function(client)
+    {
+        let json = {
+            "context": {
+                "client": client,
+                "ts": luxon.DateTime.utc()
+            }
+        }
+        let result = JSON.stringify(json);
+        return result;
+    }
+
+    return {
+        "getContext": getContext
+    };
+}());
+
+
+let client  = mqtt.connect(`${config.mqttBroker.host}:${config.mqttBroker.port}`);
 
 client.on("connect",
     function ()
     {
+        client.subscribe(config.jamulus.topicPingAck,
+            function (err)
+            {
+                if (err)
+                {
+                    console.error(`Error subscribing to topic ${config.jamulus.topicPingAck}`);
+                    console.error(err);
+                }
+            });
+
         config.logging.topics.forEach(
             function(topic)
             {
@@ -24,19 +54,30 @@ client.on("connect",
         setTimeout(
             function()
             {
-                // TODO
-                //  - Ping servers
+                // Ping servers
+                client.publish(config.jamulus.topicPing, internals.getContext(config.jamulus.clientId));
                 //  - request connections and config for each responding server
-                client.publish("jamulus/22124/state/request/configuration/get", "{ \"client\": \"jamulus-logger\"}");
-                client.publish("jamulus/22124/state/request/connections/get", "{ \"client\": \"jamulus-logger\"}");
-            }, 1000);
+                // client.publish("jamulus/22124/state/request/configuration/get", internals.getContext(config.jamulus.clientId));
+                // client.publish("jamulus/22124/state/request/connections/get", internals.getContext(config.jamulus.clientId));
+            }, config.jamulus.pingDelay);
     });
 
 client.on("message",
     function (topic, message)
     {
-        // message is Buffer
         let json = JSON.parse(message.toString());
-        let jsonString = JSON.stringify(json);
-        console.log(`${luxon.DateTime.utc()} ${topic} ${jsonString}`);
+        if(topic === config.jamulus.topicPingAck)
+        {
+            console.warn("json.server.type", json.server.type);
+            if(json.server.type === "state")
+            {
+                client.publish(`jamulus/${json.server.port}/state/request/configuration/get`, internals.getContext(config.jamulus.clientId));
+                client.publish(`jamulus/${json.server.port}/state/request/connections/get`, internals.getContext(config.jamulus.clientId));
+            }
+        }
+        else
+        {
+            let jsonString = JSON.stringify(json);
+            console.log(`${luxon.DateTime.utc()} ${topic} ${jsonString}`);
+        }
     })
